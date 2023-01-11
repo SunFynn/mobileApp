30139fbb6c58f89bb79258777fdc3d9e09384531
weitinzhou <weitinzhou@pinming.cn>
2023-01-11 15:51:32 +0800

    feat:嵌入taro-ui组件库

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 taro + react + typescript + taro-ui  
 
 ## grid Tab
-通过grid布局实现交叉瀑布流的方式  <br>
-grid-auto-rows: 1px;   <br>
-cardBoxDivLis[i].style.gridRowEnd = `span ${h * 2 + 30}`;  <br>
+
+通过grid布局实现交叉瀑布流的方式 
+
+grid-auto-rows: 1px;  
+
+cardBoxDivLis[i].style.gridRowEnd = `span ${h * 2 + 30}`; 
+
 
 ## IntersectionObserver Tab
-使用交叉试图模式添加交叉瀑布流  <br>
+
+使用交叉试图模式添加交叉瀑布流  
--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,56 @@
 import React, { Component, PropsWithChildren } from "react";
 import { redirectTo } from "@tarojs/taro";
+import { AtTabBar } from "taro-ui";
 import styles from "./app.module.less";
+import 'taro-ui/dist/style/index.scss';
 
-class App extends Component<PropsWithChildren> {
+class App extends Component<PropsWithChildren, any> {       
+  constructor(props){
+    super(props);
+    this.state={
+      current: 0,
+      navList: [
+        {title: "grid", url: "pages/index/index"}, 
+        {title: '照相'},
+        {title: "IntersectionObserver", url: 'pages/intersectionObserver/index'}
+      ]
+    }
+  }
+ 
   componentDidMount() {}
 
   componentDidShow() {}
 
   componentDidHide() {}
 
+  handleTabChange(value){
+    const { navList } = this.state;
+    this.setState({
+      current: value
+    });
+    redirectTo({url: navList[value].url})
+  }
+
   render() {
-    const navList = [
-      {title: "grid", url: "pages/index/index"}, 
-      {title: "IntersectionObserver", url: 'pages/intersectionObserver/index'}
-    ];
     // this.props.children 是将要会渲染的页面
     return (
       <div className={styles.appBox}>
         <div className={styles.childrenBox}>{this.props.children}</div>
         <div className={styles.bottomNavBox}>
-          <div className={styles.bottomGrid}>
-            {navList.map((item, idx) => {
+          {/* <div className={styles.bottomGrid}>
+            {this.state.navList.map((item, idx) => {
               return <div key={idx} onClick={()=>{ redirectTo({url: item.url})}}>{item.title}</div>;
             })}
-          </div>
+          </div> */}
+          <AtTabBar
+            tabList={[
+              { title: 'grid', iconType: 'bullet-list', text: 'new' },
+              { title: '拍照', iconType: 'camera' },
+              { title: 'IntersectionObserver', iconType: 'folder', text: '100', max: 99 }
+            ]}
+            onClick={this.handleTabChange.bind(this)}
+            current={this.state.current}
+          />
         </div>
       </div>
     );
